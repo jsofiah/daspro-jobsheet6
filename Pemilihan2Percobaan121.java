409ddbb9e7e239d93411c234838a51a2ffb99539
sofiahdingdong <sofiahdingdong@gmail.com>
2024-10-04 11:55:37 +0700

    percobaan 1 nomor 2

--- Pemilihan2Percobaan121.java
+++ Pemilihan2Percobaan121.java
@@ -7,10 +7,18 @@ public class Pemilihan2Percobaan121 {
         System.out.print("Masukkan tahun: ");
         tahun = input21.nextInt();
 
-        if((tahun % 4) == 0){
-            if((tahun % 100) !=0)
+        if ((tahun % 4) == 0) {
+            if ((tahun % 100) != 0) {
                 System.out.println("Tahun Kabisat");
-        } else
+            } else {
+                if ((tahun % 400) == 0) {
+                    System.out.println("Tahun Kabisat");
+                } else {
+                    System.out.println("Bukan Tahun Kabisat");
+                }
+            }
+        } else {
             System.out.println("Bukan Tahun Kabisat");
+        }
     }
 }
