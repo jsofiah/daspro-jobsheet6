e735fe0a410bd77bbdc31ea5e950c7379934d054
sofiahdingdong <sofiahdingdong@gmail.com>
2024-10-04 18:11:57 +0700

    Tugas Algoritma 2

---
+++ TugasAlgoritma2.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class TugasAlgoritma2 {
+    public static void main(String[] args) {
+        Scanner input21 = new Scanner(System.in);
+        String jenisBuku;
+        int jumlahBeli, diskon = 0;
+
+        System.out.print("Masukkan jenis buku: ");
+        jenisBuku = input21.nextLine();
+        System.out.print("Masukkan jumlah buku: ");
+        jumlahBeli = input21.nextInt();
+
+        if(jenisBuku.equalsIgnoreCase("Kamus")){
+            diskon = 10;
+            if(jumlahBeli > 2)
+                diskon += 2;
+            else
+                diskon +=0;
+        }
+        else if(jenisBuku.equalsIgnoreCase("Novel")){
+            diskon = 7;
+            if(jumlahBeli > 3)
+                diskon += 2;
+            else if(jumlahBeli <= 3)
+                diskon += 1;
+            else 
+                diskon +=0;
+        }
+        else{
+            if(jumlahBeli >3)
+                diskon = 5;
+            else 
+                diskon = 0;
+        }
+
+        System.out.println("Diskon yang didapat sebesar " + diskon + " %");
+    }
+}
