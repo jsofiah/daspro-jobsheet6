b66b45a7a22f2a4ba1b0b4ec3b7944669c38840d
sofiahdingdong <sofiahdingdong@gmail.com>
2024-10-09 19:19:29 +0700

    Tugas Algoritma 3 Sempurna

--- TugasAlgoritma3.java
+++ TugasAlgoritma3.java
@@ -13,62 +13,83 @@ public class TugasAlgoritma3 {
         System.out.print("Masukkan ukuran: ");
         ukuran = input21.nextInt();
 
-        if(merk.equalsIgnoreCase("Converse")){
-            if(kategori.equalsIgnoreCase("Slip On")){
-                if(ukuran >= 36 && ukuran <=40)
-                    harga = 800000;
-                else{
+        if(merk.equalsIgnoreCase("Converse")) {
+            if(kategori.equalsIgnoreCase("Slip On")) {
+                if(ukuran >= 36) {
+                    if(ukuran <= 40) {
+                        harga = 800000;
+                    } else {
+                        System.out.println("Ukuran tidak tersedia");
+                    }
+                } else {
                     System.out.println("Ukuran tidak tersedia");
                 }
-            }
-            else if(kategori.equalsIgnoreCase("High Top")){
-                if(ukuran >= 36 && ukuran <= 44)
-                    harga = 1200000;
-                else{
+            } else if(kategori.equalsIgnoreCase("High Top")) {
+                if(ukuran >= 36) {
+                    if(ukuran <= 44) {
+                        harga = 1200000;
+                    } else {
+                        System.out.println("Ukuran tidak tersedia");
+                    }
+                } else {
                     System.out.println("Ukuran tidak tersedia");
                 }
-            }
-            else{
+            } else {
                 System.out.println("Kategori tidak tersedia");
             }
-        }
-        else if(merk.equalsIgnoreCase("Sketcher")){
-            if(kategori.equalsIgnoreCase("Woman")){
-                if(ukuran >=36 && ukuran <=41)
-                    harga= 1000000;
-                else{
+        } else if(merk.equalsIgnoreCase("Sketcher")) {
+            if(kategori.equalsIgnoreCase("Woman")) {
+                if(ukuran >= 36) {
+                    if(ukuran <= 41) {
+                        harga = 1000000;
+                    } else {
+                        System.out.println("Ukuran tidak tersedia");
+                    }
+                } else {
                     System.out.println("Ukuran tidak tersedia");
                 }
-            }
-            else if(kategori.equalsIgnoreCase("Man")){
-                if(ukuran >=41 && ukuran <= 44)
-                    harga = 1800000;
-                else{
+            } else if(kategori.equalsIgnoreCase("Man")) {
+                if(ukuran >= 41) {
+                    if(ukuran <= 44) {
+                        harga = 1800000;
+                    } else {
+                        System.out.println("Ukuran tidak tersedia");
+                    }
+                } else {
                     System.out.println("Ukuran tidak tersedia");
                 }
+            } else {
+                System.out.println("Kategori tidak tersedia");
             }
-        }
-        else if(merk.equalsIgnoreCase("Nike")){
-            if(kategori.equalsIgnoreCase("Kids")){
-                if(ukuran >= 36 && ukuran <=40)
-                    harga = 750000;
-                else{
+        } else if(merk.equalsIgnoreCase("Nike")) {
+            if(kategori.equalsIgnoreCase("Kids")) {
+                if(ukuran >= 36) {
+                    if(ukuran <= 40) {
+                        harga = 750000;
+                    } else {
+                        System.out.println("Ukuran tidak tersedia");
+                    }
+                } else {
                     System.out.println("Ukuran tidak tersedia");
                 }
-            }
-            else if(kategori.equalsIgnoreCase("Adult")){
-                if(ukuran >= 40 && ukuran <= 44)
-                    harga = 1500000;
-                else{
+            } else if(kategori.equalsIgnoreCase("Adult")) {
+                if(ukuran >= 40) {
+                    if(ukuran <= 44) {
+                        harga = 1500000;
+                    } else {
+                        System.out.println("Ukuran tidak tersedia");
+                    }
+                } else {
                     System.out.println("Ukuran tidak tersedia");
                 }
+            } else {
+                System.out.println("Kategori tidak tersedia");
             }
-        }
-        else {
+        } else {
             System.out.println("Merk tidak tersedia");
         }
-    
-    System.out.println("Pembelian " + merk + " kategori " + kategori + " dengan ukuran " + ukuran);
-    System.out.println("Total pembelian : " + harga);
-}
+
+        System.out.println("Pembelian " + merk + " kategori " + kategori + " dengan ukuran " + ukuran);
+        System.out.println("Total pembelian : " + harga);
+    }
 }
